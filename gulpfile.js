73b146794e26f7e230e53d98f417477e12d5988f
Felipe <felipegdas07@gmail.com>
2018-10-25 11:30:45 -0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'watch', 'imagemin']);
 
 gulp.task('sass', function(){
-    return gulp.src('./src/assets/scss/style.scss')
+    return gulp.src('./src/assets/scss/main.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./dist/assets/css/'));
 });
 
 gulp.task('watch', function(){
     gulp.watch('./src/assets/scss/*.scss',['sass']);
+});
+
+gulp.task('imagemin', function(){
+    gulp.src('./src/assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/assets/img/'));
 });
